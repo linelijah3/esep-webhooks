f920bdbfd453db89c875f843214157a4ed9bb60e
linelijah3 <elijahlin3@gmail.com>
2023-11-11 23:23:26 -0500

    push7

--- Function.cs
+++ Function.cs
@@ -5,7 +5,7 @@ using Newtonsoft.Json;
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
-namespace Esep-Webhook;
+namespace EsepWebhook;
 
 public class Function
 {
